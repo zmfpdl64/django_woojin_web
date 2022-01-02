4612db1f82f08b37f7e4cfe5bc7bf08487988086
woojin <zmfpdl64@naver.com>
2022-01-03 01:07:51 +0900

    구글 로그인 기능 추가하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -37,12 +37,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
-    'single_pages',
-    'django_extensions',
 
     'crispy_forms',
     'markdownx',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'django.contrib.sites',
+
+    'blog',
+    'single_pages',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +110,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    
 ]
+
+
 
 
 # Internationalization
@@ -134,3 +144,15 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+
+    'allauth.account.auth_backends.AuthenticationBackend',
+
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+LOGIN_REDIRECT_URL = '/blog/'
--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('', include('single_pages.urls')),
     path('markdownx/', include('markdownx.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
