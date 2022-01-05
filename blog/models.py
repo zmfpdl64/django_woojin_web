b95ef7aca30b9ca4942d1acb8507e1045b4104da
woojin <zmfpdl64@naver.com>
2022-01-06 03:35:40 +0900

    대문 페이지 수정하기

--- blog/models.py
+++ blog/models.py
@@ -57,6 +57,12 @@ class Post(models.Model):
 
     def get_content_markdown(self):
         return markdown(self.content)
+    
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/521/3a0724f045e8b161/svg/{ self.author.email }'
 
 
 class Comment(models.Model):
--- single_pages/tests.py
+++ single_pages/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from bs4 import BeautifulSoup
+from blog.models import Post
 
-# Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user_trump = User.objects.create_user(username='trump', password='somepassword')
+
+        self.post_001 = Post.objects.create(
+            title="첫 번째 포스트",
+            content = "첫 번째 포스트입니다.",
+            author = self.user_trump
+        )
+
+        self.post_002 = Post.objects.create(
+            title="두 번째 포스트",
+            content = "두 번째 포스트입니다.",
+            author = self.user_obama
+        )
+        self.post_003 = Post.objects.create(
+            title="세 번째 포스트",
+            content = "세 번째 포스트입니다.",
+            author = self.user_obama
+        )
+        self.post_004 = Post.objects.create(
+            title="네 번째 포스트",
+            content = "네 번째 포스트입니다.",
+            author = self.user_obama
+        )
