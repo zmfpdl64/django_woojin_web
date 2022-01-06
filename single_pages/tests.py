3f25e94f4eeca1e1d1caffff324ae9e21dfced29
woojin <zmfpdl64@naver.com>
2022-01-06 16:58:02 +0900

    자기소개 페이지에 내용 채우기

--- single_pages/tests.py
+++ single_pages/tests.py
@@ -8,24 +8,35 @@ class TestView(TestCase):
         self.client = Client()
         self.user_trump = User.objects.create_user(username='trump', password='somepassword')
 
-        self.post_001 = Post.objects.create(
+        post_001 = Post.objects.create(
             title="첫 번째 포스트",
             content = "첫 번째 포스트입니다.",
             author = self.user_trump
         )
 
-        self.post_002 = Post.objects.create(
+        post_002 = Post.objects.create(
             title="두 번째 포스트",
             content = "두 번째 포스트입니다.",
-            author = self.user_obama
+            author = self.user_trump
         )
-        self.post_003 = Post.objects.create(
+        post_003 = Post.objects.create(
             title="세 번째 포스트",
             content = "세 번째 포스트입니다.",
-            author = self.user_obama
+            author = self.user_trump
         )
-        self.post_004 = Post.objects.create(
+        post_004 = Post.objects.create(
             title="네 번째 포스트",
             content = "네 번째 포스트입니다.",
-            author = self.user_obama
+            author = self.user_trump
         )
+
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        body= soup.body
+        self.assertNotIn(post_001.title, body.text)
+        self.assertNotIn(post_002.title, body.text)
+        self.assertNotIn(post_003.title, body.text)
+        self.assertNotIn(post_004.title, body.text)
+        
