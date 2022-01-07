8cf4610582570cb8d39a40a440cf98c674e88bc7
woojin <zmfpdl64@naver.com>
2022-01-07 16:40:10 +0900

    Nginx 적용하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -139,7 +139,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
